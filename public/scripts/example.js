6f90bc01d0fe3fd1701c6e2b3176ff2cea1cd9f5
koduki <koduki@kodukis-MacBook-Air.local>
2015-10-12 20:44:47 +0900

    support state.

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,3 +1,4 @@
+
 let todos = [{
   id: '_1',
   name: 'Buy some milk',
@@ -30,22 +31,37 @@ class TodoList extends React.Component {
 }
 
 class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name : ''}
+  }
+
+  handleNameChange(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    const name = this.refs.todoName.getDOMNoode().value.trim();
-    if(name) {
-      alert(name);
-      this.refs.todoName.getDOMNode().value = '';
-    }
+    const name = this.state.name.trim();
+    alert(name);
+    this.setState({
+      name: ''
+    });
   }
+
   render() {
+    let disabled = this.state.name.trim().length <= 0;
     return (
-      <form onSubmit="this.handleSubmit">
-        <input ref="todoName"></input><input type="submit"></input>
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <input value={this.state.name} onChange={this.handleNameChange.bind(this)}></input>
+        <input type="submit"></input>
       </form>
     );
   }
 }
+
 class App extends React.Component {
   render() {
     return (
